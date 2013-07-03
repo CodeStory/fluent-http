14e184de2f936169e0cb5e99d7167ba158b834e2
dgageot <david@gageot.net>
2013-07-03 18:46:57 +0200

    Don't let LessCompiler decide when to compile

--- src/main/java/net/codestory/http/compilers/LessCompiler.java
+++ src/main/java/net/codestory/http/compilers/LessCompiler.java
@@ -1,6 +1,7 @@
 package net.codestory.http.compilers;
 
 import java.io.*;
+import java.nio.charset.*;
 import java.nio.file.*;
 
 import com.github.sommeri.less4j.*;
@@ -9,7 +10,9 @@ import com.github.sommeri.less4j.core.*;
 public class LessCompiler {
   public String compile(Path path) throws IOException {
     try {
-      return new ThreadUnsafeLessCompiler().compile(path.toFile()).getCss();
+      String less = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+
+      return new ThreadUnsafeLessCompiler().compile(less).getCss();
     } catch (Less4jException e) {
       throw new IOException("Unable to compile less file", e);
     }
