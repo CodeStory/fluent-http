cd9a33c25bb552d7e80ccdb79a9199395face49b
dgageot <david@gageot.net>
2013-07-12 13:59:17 +0200

    Use another library for Markdown to html

--- src/test/java/net/codestory/http/compilers/MarkdownCompilerTest.java
+++ src/test/java/net/codestory/http/compilers/MarkdownCompilerTest.java
@@ -2,22 +2,20 @@ package net.codestory.http.compilers;
 
 import static org.fest.assertions.Assertions.*;
 
-import java.io.*;
-
 import org.junit.*;
 
 public class MarkdownCompilerTest {
   MarkdownCompiler markdownCompiler = new MarkdownCompiler();
 
   @Test
-  public void empty() throws IOException {
+  public void empty() {
     String html = markdownCompiler.compile("");
 
     assertThat(html).isEmpty();
   }
 
   @Test
-  public void markdown_to_html() throws IOException {
+  public void markdown_to_html() {
     String css = markdownCompiler.compile("This is **bold**");
 
     assertThat(css).isEqualTo("<p>This is <strong>bold</strong></p>");
