2c35c6e6d533af29dc325571a43019d5f1861f06
David Gageot <david@gageot.net>
2014-01-15 09:04:14 +0100

    Fix visibility

--- src/main/java/net/codestory/http/compilers/MarkdownCompiler.java
+++ src/main/java/net/codestory/http/compilers/MarkdownCompiler.java
@@ -22,7 +22,7 @@ import org.markdown4j.*;
 
 import com.github.rjeschke.txtmark.*;
 
-public class MarkdownCompiler implements Compiler {
+class MarkdownCompiler implements Compiler {
   private final Configuration configuration;
 
   public MarkdownCompiler() {
--- src/main/java/net/codestory/http/compilers/TablePlugin.java
+++ src/main/java/net/codestory/http/compilers/TablePlugin.java
@@ -19,7 +19,7 @@ import java.util.*;
 
 import org.markdown4j.*;
 
-public class TablePlugin extends Plugin {
+class TablePlugin extends Plugin {
   public TablePlugin() {
     super("table");
   }
