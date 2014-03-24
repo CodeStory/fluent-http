b31ce611e7ef3423dfa3a37fb9a04391df1b4b4b
David Gageot <david@gageot.net>
2014-03-24 19:08:14 +0100

    Upgrade Coffee

--- src/main/java/net/codestory/http/compilers/CoffeeCompiler.java
+++ src/main/java/net/codestory/http/compilers/CoffeeCompiler.java
@@ -20,7 +20,7 @@ import java.nio.file.*;
 
 class CoffeeCompiler implements Compiler {
   private final NashornCompiler nashornCompiler = new NashornCompiler(
-      "META-INF/resources/webjars/coffee-script/1.7.0/coffee-script.min.js",
+      "META-INF/resources/webjars/coffee-script/1.7.1/coffee-script.min.js",
       "coffee-script/compile.js");
 
   @Override
