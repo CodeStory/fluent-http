9bad1c76bc556a90a85ec1c183f6d76135e34f5c
David Gageot <david@gageot.net>
2014-05-06 13:14:16 +0200

    MixedAuthFilter

---
+++ src/main/java/net/codestory/http/filters/mixed/MixedAuthFilter.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2013 all@code-story.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+package net.codestory.http.filters.mixed;
+
+import static net.codestory.http.constants.Headers.*;
+
+import java.io.*;
+
+import net.codestory.http.exchange.*;
+import net.codestory.http.filters.*;
+import net.codestory.http.filters.auth.*;
+import net.codestory.http.filters.basic.*;
+import net.codestory.http.payload.*;
+import net.codestory.http.security.*;
+
+public class MixedAuthFilter implements Filter {
+  private final Filter cookieAuthFilter;
+  private final Filter basicAuthFilter;
+
+  public MixedAuthFilter(String uriPrefix, String realm, Users users, SessionIdStore sessionIdStore) {
+    this.cookieAuthFilter = new CookieAuthFilter(uriPrefix, users, sessionIdStore);
+    this.basicAuthFilter = new BasicAuthFilter(uriPrefix, realm, users);
+  }
+
+  @Override
+  public boolean matches(String uri, Context context) {
+    if (context.header(AUTHORIZATION) != null) {
+      return basicAuthFilter.matches(uri, context);
+    } else {
+      return cookieAuthFilter.matches(uri, context);
+    }
+  }
+
+  @Override
+  public Payload apply(String uri, Context context, PayloadSupplier nextFilter) throws IOException {
+    if (context.header(AUTHORIZATION) != null) {
+      return basicAuthFilter.apply(uri, context, nextFilter);
+    } else {
+      return cookieAuthFilter.apply(uri, context, nextFilter);
+    }
+  }
+}
