66fc2b53befa94b237bcf3f6651b0ad649d77cb7
David Gageot <david@gageot.net>
2014-05-21 19:41:08 +0200

    Trying to fix simple and cookies

--- src/main/java/net/codestory/http/internal/SimpleResponse.java
+++ src/main/java/net/codestory/http/internal/SimpleResponse.java
@@ -15,12 +15,20 @@
  */
 package net.codestory.http.internal;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.SimpleTimeZone;
+import java.util.TimeZone;
 
 import net.codestory.http.Cookie;
 import net.codestory.http.Response;
 
-import org.simpleframework.http.*;
+import org.simpleframework.http.Status;
 
 class SimpleResponse implements Response {
   private final org.simpleframework.http.Response response;
@@ -56,7 +64,19 @@ class SimpleResponse implements Response {
 
   @Override
   public void setCookie(Cookie newCookie) {
-    org.simpleframework.http.Cookie cookie = new org.simpleframework.http.Cookie(newCookie.name(), newCookie.value(), newCookie.path(), newCookie.isNew());
+    CookieDate cookieDate = new CookieDate();
+    org.simpleframework.http.Cookie cookie = new org.simpleframework.http.Cookie(newCookie.name(), newCookie.value(), newCookie.path(), newCookie.isNew()) {
+      @Override
+      public String toString() {
+        return getName() + "=" + getValue() + "; version=" +
+          getVersion() + (getPath() == null ? "" : "; path=" + getPath()) +
+          (getDomain() == null ? "" : "; domain=" + getDomain()) +
+          (getExpiry() < 0 ? "" : "; expires=" + cookieDate.format(getExpiry())) +
+          (getExpiry() < 0 ? "" : "; max-age=" + getExpiry()) +
+          (isSecure() ? "; secure" : "") +
+          (isProtected() ? "; httponly" : "");
+      }
+    };
     cookie.setExpiry(newCookie.expiry());
     cookie.setVersion(newCookie.version());
     cookie.setSecure(newCookie.isSecure());
@@ -70,5 +90,36 @@ class SimpleResponse implements Response {
   @SuppressWarnings("unchecked")
   public <T> T unwrap(Class<T> type) {
     return type.isInstance(response) ? (T) response : null;
+  }
+
+  private static class CookieDate {
+    private static final String FORMAT = "EEE, dd-MMM-yyyy HH:mm:ss z";
+    private static final String ZONE = "GMT";
+
+    private final DateFormat format;
+    private final TimeZone zone;
+
+    public CookieDate() {
+      this.format = new SimpleDateFormat(FORMAT, Locale.US);
+      this.zone = new SimpleTimeZone(0, ZONE);
+    }
+
+    public String format(int seconds) {
+      Calendar calendar = Calendar.getInstance(zone, Locale.US);
+      Date date = convert(seconds);
+
+      calendar.setTime(date);
+      format.setCalendar(calendar);
+
+      return format.format(date);
+    }
+
+    private Date convert(int seconds) {
+      long now = System.currentTimeMillis();
+      long duration = seconds * 1000L;
+      long time = now + duration;
+
+      return new Date(time);
+    }
   }
 }
