2f2f52f78c11d3e1e9cfac0bc4aa7723bcb8a683
David Gageot <david@gageot.net>
2015-03-17 11:58:33 +0100

    Should extract the inputStream

--- src/main/java/net/codestory/http/Context.java
+++ src/main/java/net/codestory/http/Context.java
@@ -132,6 +132,9 @@ public class Context {
       if (clazz.isAssignableFrom(String.class)) {
         return request.content();
       }
+      if (clazz.isAssignableFrom(InputStream.class)) {
+        return request.inputStream();
+      }
       if (clazz.isAssignableFrom(Site.class)) {
         return site;
       }
